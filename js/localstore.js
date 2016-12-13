5654bd7599194bf1fcca630b7b571702d2f67b4f
Stuart Reeve <P13201388@LEC-ADMIN.dmu.ac.uk>
2016-12-13 11:43:25 +0000

    Added field to store course in local storge

--- js/localstore.js
+++ js/localstore.js
@@ -1,10 +1,13 @@
 (function(){
 
   let myName = document.getElementById("my-name"); 
+  let myCourse = document.getElementById("my-course");
   let getName = document.getElementById("get-name");
   let userName = document.getElementById("user-name"); 
+  let courseName = document.getElementById("course-name");
   let clearName = document.getElementById("clear-name");
   let nameStored = localStorage.name;
+  let courseStored = localStorage.course;
   console.log(`Name on page load: ${nameStored}`);
   
   if(nameStored) {
@@ -17,19 +20,36 @@
     myName.innerHTML = "stranger";
     console.log(`No name stored`);
   }
+  
+  if(courseStored) {
+	// if course then use it
+	myCourse.innerHTML = `${courseStored}`;
+	console.log(`Course stored is {nameStored}`);
+  }
+  else {
+	myCourse.innerHTML = "No Course";
+    console.log(`No course stored`);
+  }
 
   function PerformGreeting() {
     if(userName.value === "") {
       alert("Please enter a name");
       userName.focus();
-    }
-    // Get the name the user entered:
+    } else if(courseName.value === "") {
+	  alert("Please enter a course");
+	  courseName.focus();
+	}
+    // Get values the user entered:
     nameStored = userName.value;
+	courseStored = courseName.value;
     // Show the name in "my-name":
     myName.innerHTML = nameStored;
+	myCourse.innerHTML = courseStored;
     // Put the name into localStorage:
     localStorage.name = nameStored;
+	localStorage.course = courseStored;
     console.log(`New name stored: ${nameStored}`);
+	console.log(`New course stored: $(courseStored}`);
     return false;
   }
   
